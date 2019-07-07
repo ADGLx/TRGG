4fcd16a2360ba791dcf439fd34e16ba034381eca
Alvaro Gonzalez <alvarodavidglx@hotmail.com>
2019-07-07 13:02:15 -0400

    Created the move Anim

--- Actual Files/TurnGeneratedBox/Assets/_Scripts/Unit.cs
+++ Actual Files/TurnGeneratedBox/Assets/_Scripts/Unit.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.Tilemaps;
+using System.Collections;
 
 public class Unit : MonoBehaviour {
 
@@ -16,7 +17,8 @@ public class Unit : MonoBehaviour {
 
     public Vector2Int GridPos = new Vector2Int();
     public UnitIn Type = UnitIn.Player;
-   // public Tile A; no clue what this was for 
+    // public Tile A; no clue what this was for 
+    public float AnimSpeed = 1f;
     public GameObject Target;
     private GameObject TempParticles;
 
@@ -44,22 +46,13 @@ public class Unit : MonoBehaviour {
     }
     void Start()
     {
-         GetPos();
         SetPos(GridPos.x, GridPos.y);
 
-      //  MoveUnitTo(-5, -3); this is making it so when the pathfinding happens the thing is set as not occupied
+        //  MoveUnitTo(-5, -3); this is making it so when the pathfinding happens the thing is set as not occupied
+        MoveUnitTo(4, -4);
 
     }
 
-    void GetPos()
-    {
-        float PosX, PosY;
-
-        PosX = this.transform.position.x;
-        PosY = this.transform.position.y;
-
-        GridPos = new Vector2Int(Convert.ToInt32(PosX), Convert.ToInt32(PosY - 1));
-    }
 
     public void SetPos(int X, int Y)
     {
@@ -85,12 +78,30 @@ public class Unit : MonoBehaviour {
         List<MapTile> MyPath = new List<MapTile>();
         Vector2Int DebugPos = new Vector2Int(TargetX, TargetY);
         MyPath = MapLocal.Pathfinding(GridPos, new Vector2Int(DebugPos.x, DebugPos.y));
+      //  StartCoroutine(PathMoveAnim(MyPath));
+        StartCoroutine(MoveToTileAnim(MyPath));
 
         //This should be in another function to be more organized tho
-        foreach(MapTile T in MyPath)
+
+        //  PlaceTarget(DebugPos.x, DebugPos.y); Add this somewhere else
+    }
+
+
+    IEnumerator MoveToTileAnim(List<MapTile> thPath)
+    {
+        float step = AnimSpeed * Time.deltaTime;
+
+        foreach (MapTile T in thPath)
         {
-            Debug.Log(T.X + " / " + T.Y);
+            while (this.transform.position != MapLocal.SetTilePosToWorld(T.X, T.Y)) //this is dangerous
+            {
+                this.transform.position = Vector2.MoveTowards(this.transform.position, MapLocal.SetTilePosToWorld(T.X, T.Y), step);
+                yield return null;
+            }
+            SetPos(T.X, T.Y);
         }
-        PlaceTarget(DebugPos.x, DebugPos.y);
+
+
+
     }
 }
